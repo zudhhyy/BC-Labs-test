2817c02c4ce6a4025dc6579b1b6505f0802781d4
zudhhyy <tubagus.zuhdi2@gmail.com>
2025-01-21 22:41:47 +0700

    check error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import Footer from '@/components/footer/Footer';
 import HeroHome from '@/components/hero/HeroHome';
 
 const Home = async () => {
-  const res = await fetch('http://localhost:3000/api/data');
-  const homeData = await res.json();
+  // const res = await fetch('http://localhost:3000/api/data');
+  // const homeData = await res.json();
 
   return (
     <div>
       <div className="flex flex-col bg-hero bg-center pb-[60px] text-white lg:bg-[length:100%_100%] lg:pb-[108px]">
         <Navbar />
-        <HeroHome homeData={homeData} />
+        {/* <HeroHome homeData={homeData} /> */}
       </div>
-      <Footer companyDetail={homeData.companyDetail} footerMenu={homeData.footerMenu} />
+      {/* <Footer companyDetail={homeData.companyDetail} footerMenu={homeData.footerMenu} /> */}
     </div>
   );
 };
